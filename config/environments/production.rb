e036aac7ad7b1f4a2bc7af716f661742d814fe30
Stephen Prosser <stephenaprosser@gmail.com>
2014-02-23 21:46:30 -0600

    mobile and performance enhancements

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,8 +38,8 @@ DaylightDistrict::Application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-  config.action_dispatch.x_sendfile_header = nil 
-  
+  config.action_dispatch.x_sendfile_header = nil
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
@@ -74,7 +74,7 @@ DaylightDistrict::Application.configure do
   config.active_support.deprecation = :notify
 
   # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
+  config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
